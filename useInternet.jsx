7e60fc4a4605cac01d8fe8982cd33c3fddbda677
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 <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>
2024-01-13 14:29:35 +0530

    useInternet Custom Hook

--- useInternet.jsx
+++ useInternet.jsx
@@ -24,3 +24,19 @@ const useOnline = () => {
 };
 
 export default useOnline;
+
+// Before using this, you should include the condition below wherever necessary and also import it into your component.
+
+/*
+import useOnline from "./path/useOnline";
+
+const isOnline = useOnline();
+
+if (!isOnline) {
+  return (
+    <>
+      <h1 className="no-internet">Check your Internet Connection</h1>
+    </>
+  );
+}
+*/
